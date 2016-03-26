caedac10f9a83202b3da8ac718ee8c7ca30561c2
Alessio Linares <alessio@alessio.cc>
2016-03-26 14:38:08 +0100

    More testing

--- src/main.cpp
+++ src/main.cpp
@@ -32,23 +32,13 @@ int main(void)
     sf::Texture tex2;
     tex2.loadFromFile("./sprites.png");
     mogl::MultimediaOGL* mogl = g.addPlugin<mogl::MultimediaOGL>(sf::VideoMode(600, 600), "Test", sf::Style::Default, settings);
-    glEnable(GL_DEPTH_TEST);
     g.addPlugin<h2d::KinematicWorld>();
-    //mogl->setProjection(glm::perspective(90.f, 1.f, 0.1f, 100.0f));
-    mogl->setProjection(glm::ortho(-40.f, 40.f, -40.f, 40.f, 0.1f, 100.f));
-    mogl->setView(glm::lookAt(glm::vec3(0, 0, 1), glm::vec3(0, 0, -1), glm::vec3(0, 1, 0)));
+    mogl->setProjection(glm::perspective(100.f, 1.f, 0.1f, 200.0f));
+    //mogl->setProjection(glm::ortho(-40.f, 40.f, -40.f, 40.f, 0.1f, 100.f));
+    mogl->setView(glm::lookAt(glm::vec3(0, 50, 100), glm::vec3(0, 0, -1), glm::vec3(0, -1, 0)));
     h2d::Kinematic* k;
     h2d::Actor* a = g.makeActor();
     sf::Color c;
-    c = sf::Color::Magenta;
-    c.a = 127;
-    a->addBehavior<mogl::Rectangle>(20, 20, c);
-    a->transform().z = -10;
-    k = a->addBehavior<h2d::Kinematic>();
-    //k->velocity().x = 5;
-    //k->velocity().y = 5;
-    k->velocity().rotation = 45;
-    a = g.makeActor();
     c = sf::Color::Cyan;
     c.a = 127;
     a->addBehavior<mogl::Rectangle>(30, 10, c);
@@ -58,8 +48,18 @@ int main(void)
     //k->velocity().y = 2.5;
     k->velocity().rotation = -45;
     a = g.makeActor();
+    c = sf::Color::Magenta;
+    c.a = 127;
+    auto spr =a->addBehavior<mogl::Sprite>(20, 20, &tex);
+    spr->setColor(c);
+    a->transform().z = -10;
+    k = a->addBehavior<h2d::Kinematic>();
+    //k->velocity().x = 5;
+    //k->velocity().y = 5;
+    k->velocity().rotation = 45;
+    a = g.makeActor();
     a->addBehavior<mogl::Sprite>(20, 20, &tex);
-    a->transform().z = -0;
+    a->transform().z = -50;
     k = a->addBehavior<h2d::Kinematic>();
     //k->velocity().x = 1.25;
     //k->velocity().y = 1.25;
@@ -69,11 +69,10 @@ int main(void)
         &tex2,
         0, 0, 0, 0, 48, 48,
         {0, 1, 2, 3, 5, 6, 7, 8},
-        std::vector<h2d::Time>(8, h2d::Time::milliseconds(500))
+        std::vector<h2d::Time>(8, h2d::Time::milliseconds(300))
     };
+    a->transform().z = -15;
     a->addBehavior<mogl::AnimatedSprite>(20, 20, &animation);
-    a->transform().x = 30;
-    a->transform().y = 30;
     k = a->addBehavior<h2d::Kinematic>();
     a->addBehavior<OnMouse>();
     //k->velocity().x = 1.25;
